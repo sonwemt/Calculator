4c96e2a8606ab469d805bca0b2feb824d78c9c7e
Sondre <sonfjell@gmail.com>
2022-04-29 20:12:47 +0200

    Create eventlisteners and associated functions

---
+++ logic.js
@@ -0,0 +1,97 @@
+
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+function operate(n1, n2, operator) {
+    switch(operator) {
+        case '+': return add(n1, n2);
+        case '-': return subtract(n1, n2);
+        case '*': return multiply(n1, n2);
+        case '/': return divide(n1, n2);
+        default: 
+            return 'not a valid operator';
+    }
+}
+
+function clearDisplay(){
+    display.textContent = '0';
+}
+
+function clearInput() {
+    console.log('clearhit');
+    clearDisplay();
+    currentInput.length = 0;
+    savedInput.length = 0;
+    savedOperator = 0;
+}
+
+function populateDisplay(input) {
+    display.textContent = currentInput.join('');
+}
+
+function saveInput(e) {
+    console.log('saveinputtriggered')
+    const input = currentInput.push(e.target.id);
+    populateDisplay(input);
+}
+
+function operatorClick(e) {
+    console.log('operatorclick');
+    savedInput = currentInput.slice();
+    currentInput.length = 0;
+    savedOperator = e.target.id;
+}
+
+
+
+let currentInput = [];
+let savedInput = [];
+let savedOperator = 0;
+const display = document.querySelector('.display');
+const numberButtons = document.querySelectorAll('button.number');
+const operatorButtons = document.querySelectorAll('button.operator');
+const clearButton = document.querySelector('button.action.clear');
+const equalButton = document.querySelector('button.action.equals');
+
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', saveInput);
+});
+
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', operatorClick);
+});
+
+clearButton.addEventListener('click', clearInput);
+equalButton.addEventListener('click', () =>{
+    if(savedOperator !== 0){
+        const n1 = parseInt(savedInput.join(''));
+        console.log(n1);
+        const n2 = parseInt(currentInput.join(''));
+        console.log(n2);
+        const result = operate(n1, n2, savedOperator);
+        currentInput.length = 0;
+        currentInput.push(result);
+        clearDisplay();
+        populateDisplay();
+        
+        console.log(result);
+    }
+});
+
+
+
